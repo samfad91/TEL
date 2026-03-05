95c07f2b0887a747af9126824acf0d2ea730284f
Samiha <33848351+samfad91@users.noreply.github.com>
2026-03-05 11:53:10 +0100

    commentaire

--- script.js
+++ script.js
@@ -29,18 +29,19 @@ const form = document.getElementById("contactForm");
 
 if (form) {
     form.addEventListener("submit", function (e) {
-        e.preventDefault();
+      //  e.preventDefault();
 
-        const email = document.getElementById("email").value;
-        const subject = document.getElementById("subject").value;
-        const message = document.getElementById("message").value;
+      //  const email = document.getElementById("email").value;
+      //  const subject = document.getElementById("subject").value;
+       // const message = document.getElementById("message").value;
 
-        const mailtoLink = `mailto:telcontactesi@gmail.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(
-            "From: " + email + "\n\n" + message
-        )}`;
+       // const mailtoLink = `mailto:telcontactesi@gmail.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(
+        //    "From: " + email + "\n\n" + message
+       // )}`;
 
-        window.location.href = mailtoLink;
+       // window.location.href = mailtoLink;
     });
 }
+
 
 
